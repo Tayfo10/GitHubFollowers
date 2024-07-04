6ffae442a9de84ecab7d6913814a814188f056ba
Tayfun Sener <tayfunsener2@gmail.com>
2024-07-04 12:35:57 +0300

    first commit

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Tayfun Sener on 2.04.2024.
+//
+
+import Foundation
+
+struct Follower: Codable, Hashable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Tayfun Sener on 1.04.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor =  .systemBlue
+    }
+    
+
+    
+}
